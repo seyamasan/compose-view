245e30898b7212790aebdbea3056814aa9b0f9a3
seyamasan <kst_apr_24@icloud.com>
2024-10-12 18:46:52 +0900

    ホームにリサイクラービューとカードを追加してみた

--- app/src/main/java/com/example/composeview/HomeView.kt
+++ app/src/main/java/com/example/composeview/HomeView.kt
@@ -1,10 +1,17 @@
 package com.example.composeview
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,15 +20,22 @@ import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.composeview.navigator.Screens
 import com.example.composeview.ui.theme.ComposeViewTheme
 
+val screenList = listOf(
+    Screens.First("First Viewだよ"),
+    Screens.Second("Second Viewだよ")
+)
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeView(navController: NavHostController?) {
+fun HomeView(navController: NavHostController?, modifier: Modifier = Modifier) {
+    // Scaffoldを使ってtopBarを表示 & innerPaddingをViewのpaddingに適応でbarと重ならないようにしている
     Scaffold(
         containerColor = MaterialTheme.colorScheme.background,
         topBar = {
@@ -34,17 +48,57 @@ fun HomeView(navController: NavHostController?) {
             )
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+        LazyColumn(
+            modifier = modifier
+                .padding(innerPadding)
         ) {
-            Button(onClick = { navController?.navigate(Screens.First("First Viewだよ")) }) {
-                Text(text = "ファーストビュー")
+            items(screenList) { screen ->
+                Card(
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.primary
+                    ),
+                    modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+                    onClick = {
+                        navController?.navigate(screen)
+                    }
+                ) {
+                    CardContent(screen)
+                }
             }
-            Button(onClick = { navController?.navigate(Screens.Second("Second Viewだよ")) }) {
-                Text(text = "セカンドビュー")
+        }
+    }
+}
+
+@Composable
+private fun CardContent(screen: Any) {
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth()
+    ) {
+        val name = when (screen) {
+            is Screens.First -> "First"
+            is Screens.Second -> "Second"
+            else -> {"null"}
+        }
+
+        Text(
+            text = name,
+            style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight.ExtraBold
+            ),
+            modifier = Modifier.weight(1f)
+        )
+
+        IconButton(
+            onClick = {
+                // 何も設定しない
             }
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                contentDescription = "Show view button"
+            )
         }
     }
 }
