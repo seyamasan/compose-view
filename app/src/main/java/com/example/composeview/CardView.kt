5b5a053380524b2e71cfc1f9eed5633e2ccb646b
seyamasan <kst_apr_24@icloud.com>
2024-10-19 22:27:25 +0900

    CardViewをYouTubeのコメントっぽくした

--- app/src/main/java/com/example/composeview/CardView.kt
+++ app/src/main/java/com/example/composeview/CardView.kt
@@ -3,10 +3,15 @@ package com.example.composeview
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
@@ -25,6 +30,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.composeview.navigator.Screens
 import com.example.composeview.ui.theme.ComposeViewTheme
@@ -57,47 +63,134 @@ fun CardView(navController: NavHostController?, description: String) {
         ) {
             Text(text = description)
 
+            // 普通のカード
             Card(
+                modifier = Modifier.fillMaxWidth(),
                 colors = CardDefaults.cardColors(
-                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                ),
-                modifier = Modifier
-                    .size(width = 240.dp, height = 100.dp)
-            ) {
-                Text(
-                    text = "Filled",
-                    modifier = Modifier
-                        .padding(16.dp)
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant
                 )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(top = 10.dp, start = 10.dp, end = 10.dp)
+                ) {
+                    // alignByBaseline()でテキストのベースラインを揃えている
+                    Text(
+                        modifier = Modifier.alignByBaseline(),
+                        text = stringResource(id = R.string.card_view_comment_title)
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Text(
+                        modifier = Modifier.alignByBaseline(),
+                        text = "192",
+                        fontSize = 14.sp
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .padding(top = 4.dp, start = 10.dp, end = 10.dp, bottom = 6.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Face,
+                        contentDescription = "Users",
+                        modifier = Modifier
+                            .size(30.dp)
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Text(
+                        modifier = Modifier.alignByBaseline(),
+                        text = stringResource(id = R.string.card_view_comment1),
+                        fontSize = 14.sp
+                    )
+                }
             }
 
+            // 立体感のあるカード
             ElevatedCard(
                 elevation = CardDefaults.cardElevation(
                     defaultElevation = 6.dp
                 ),
                 modifier = Modifier
-                    .size(width = 240.dp, height = 100.dp)
+                    .fillMaxWidth()
             ) {
-                Text(
-                    text = "Elevated",
+                Row(
                     modifier = Modifier
-                        .padding(16.dp)
-                )
+                        .padding(top = 10.dp, start = 10.dp, end = 10.dp)
+                ) {
+                    Text(
+                        modifier = Modifier.alignByBaseline(),
+                        text = stringResource(id = R.string.card_view_comment_title)
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Text(
+                        modifier = Modifier.alignByBaseline(),
+                        text = "19",
+                        fontSize = 14.sp
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .padding(top = 4.dp, start = 10.dp, end = 10.dp, bottom = 6.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Face,
+                        contentDescription = "Users",
+                        modifier = Modifier
+                            .size(30.dp)
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Text(
+                        modifier = Modifier.alignByBaseline(),
+                        text = stringResource(id = R.string.card_view_comment2),
+                        fontSize = 14.sp
+                    )
+                }
             }
 
+            // 枠線があるカード
             OutlinedCard(
                 colors = CardDefaults.cardColors(
                     containerColor = MaterialTheme.colorScheme.surface,
                 ),
                 border = BorderStroke(1.dp, Color.Black),
                 modifier = Modifier
-                    .size(width = 240.dp, height = 100.dp)
+                    .fillMaxWidth()
             ) {
-                Text(
-                    text = "Outlined",
+                Row(
                     modifier = Modifier
-                        .padding(16.dp)
-                )
+                        .padding(top = 10.dp, start = 10.dp, end = 10.dp)
+                ) {
+                    Text(
+                        modifier = Modifier.alignByBaseline(),
+                        text = stringResource(id = R.string.card_view_comment_title)
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Text(
+                        modifier = Modifier.alignByBaseline(),
+                        text = "57",
+                        fontSize = 14.sp
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .padding(top = 4.dp, start = 10.dp, end = 10.dp, bottom = 6.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Face,
+                        contentDescription = "Users",
+                        modifier = Modifier
+                            .size(30.dp)
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Text(
+                        modifier = Modifier.alignByBaseline(),
+                        text = stringResource(id = R.string.card_view_comment3),
+                        fontSize = 14.sp
+                    )
+                }
             }
         }
     }
